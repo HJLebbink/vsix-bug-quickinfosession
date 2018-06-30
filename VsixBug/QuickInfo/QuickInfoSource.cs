9b330fec4e50e7f84bad9623b6a509bc747c7c78
Henk-Jan Lebbink <henkjan.lebbink@gmail.com>
2018-07-01 00:02:12 +0200

    cleanup

--- VsixBug/QuickInfo/QuickInfoSource.cs
+++ VsixBug/QuickInfo/QuickInfoSource.cs
@@ -23,36 +23,15 @@ namespace QuickInfo.VsixBug
     {
         private readonly TaskScheduler _uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
 
-        //http://glyphlist.azurewebsites.net/knownmonikers/
-        private static readonly ImageId _icon = KnownMonikers.AbstractCube.ToImageId();
-
-        private ITextBuffer _textBuffer;
+        private readonly ITextBuffer _textBuffer;
         public QuickInfoSource(ITextBuffer textBuffer)
         {
             this._textBuffer = textBuffer;
         }
-        // This is called on a background thread.
-        public Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken)
-        {
-            //MyTools.Output_INFO("QuickInfoSource:AugmentQuickInfoSession");
-
-            var triggerPoint = session.GetTriggerPoint(this._textBuffer.CurrentSnapshot);
-            if (triggerPoint != null)
-            {
-                var line = triggerPoint.Value.GetContainingLine();
-                var lineNumber = triggerPoint.Value.GetContainingLine().LineNumber;
-                var lineSpan = this._textBuffer.CurrentSnapshot.CreateTrackingSpan(line.Extent, SpanTrackingMode.EdgeInclusive);
-
-               // MyTools.Output_INFO("QuickInfoSource:AugmentQuickInfoSession: triggerPoint=" + lineSpan.GetText(this._textBuffer.CurrentSnapshot));
-
-                return Testc(lineSpan, lineNumber);
-            }
-            return Task.FromResult<QuickInfoItem>(null);
-        }
 
         private QuickInfoItem RunOnUI(ITrackingSpan lineSpan)
         {
-            MyTools.Output_INFO("QuickInfoSource:DoSomethingWithUIAsync B");
+            MyTools.Output_INFO("QuickInfoSource:RunOnUI");
             var elem = new ContainerElement(
                 ContainerElementStyle.Wrapped,
                 new Expander
@@ -64,33 +43,21 @@ namespace QuickInfo.VsixBug
             return new QuickInfoItem(lineSpan, elem);
         }
 
-        private Task<QuickInfoItem> Testc(ITrackingSpan lineSpan, int lineNumber)
+        // This is called on a background thread.
+        public Task<QuickInfoItem> GetQuickInfoItemAsync(IAsyncQuickInfoSession session, CancellationToken cancellationToken)
         {
-            if (true)
+            //MyTools.Output_INFO("QuickInfoSource:AugmentQuickInfoSession");
+
+            var triggerPoint = session.GetTriggerPoint(this._textBuffer.CurrentSnapshot);
+            if (triggerPoint != null)
             {
+                var line = triggerPoint.Value.GetContainingLine();
+                var lineSpan = this._textBuffer.CurrentSnapshot.CreateTrackingSpan(line.Extent, SpanTrackingMode.EdgeInclusive);
+
                 return Task<QuickInfoItem>.Factory.StartNew(() => RunOnUI(lineSpan), CancellationToken.None, TaskCreationOptions.None, this._uiScheduler);
             }
-            else
-            {
-                var lineNumberElm = new ContainerElement(
-                    ContainerElementStyle.Wrapped,
-                    new ImageElement(_icon),
-                    new ClassifiedTextElement(
-                        new ClassifiedTextRun(PredefinedClassificationTypeNames.Keyword, "Line number: "),
-                        new ClassifiedTextRun(PredefinedClassificationTypeNames.Identifier, $"{lineNumber + 1}")
-                    ));
-
-                var dateElm = new ContainerElement(
-                    ContainerElementStyle.Stacked,
-                    lineNumberElm,
-                    new ClassifiedTextElement(
-                        new ClassifiedTextRun(PredefinedClassificationTypeNames.SymbolDefinition, "The current date: "),
-                        new ClassifiedTextRun(PredefinedClassificationTypeNames.Comment, DateTime.Now.ToShortDateString())
-                    ));
-                return Task.FromResult<QuickInfoItem>(new QuickInfoItem(lineSpan, dateElm));
-            }
+            return Task.FromResult<QuickInfoItem>(null);
         }
-
 
         public void Dispose()
         {
